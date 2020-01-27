9b2ae7e240339ba7d1bd5ea0ad600da6cdf1699f
bendevera <ben10devera@gmail.com>
2020-01-27 16:55:28 -0700

    updated gitignore

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BASILICA_KEY = "8b7c2d45-55d5-66ea-6128-9bbd53df7d7e"
